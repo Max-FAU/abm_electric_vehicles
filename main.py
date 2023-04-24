8d1f150e9e0ee1822a696c344f8d889abd336fe0
Max-FAU <maximilian_brueggemann@web.de>
2023-04-24 13:00:16 +0200

    implemented running model for multiple times

--- main.py
+++ main.py
@@ -7,28 +7,41 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     start = timeit.default_timer()
     start_date = '2008-07-13'
-    end_date = '2008-07-27'
-    num_agents = 2
+    end_date = '2008-07-14'
+    num_agents = 3
+    model_runs = 5
 
     time_diff = pd.to_datetime(end_date) - pd.to_datetime(start_date)
     num_intervals = int(time_diff / datetime.timedelta(minutes=15))
     # num_intervals = 24
-    model = ChargingModel(num_agents, start_date, end_date)
 
-    for i in range(num_intervals):
-        model.step()
+    model_results = []
+    # Run the whole model x times
+    for i in range(model_runs):
+        print("Start iteration {} of model runs...".format(i + 1))
+        model = ChargingModel(num_agents, start_date, end_date)
 
-    model_data = model.datacollector.get_model_vars_dataframe()
-    model_data['total_recharge_value'].plot(linewidth=0.5)
+        for j in range(num_intervals):
+            model.step()
+
+        model_data = model.datacollector.get_model_vars_dataframe()
+        model_results.append(model_data)
+        print("...finished iteration {} of model runs.".format(i + 1))
+
+    df_concat = pd.concat(model_results)
+    df_results = df_concat.groupby(df_concat.index).mean()
+    # df_results.to_csv("results.csv", index=False)
+
+    df_results['total_recharge_value'].plot(linewidth=0.5)
     plt.show()
-
-    agent_data = model.datacollector.get_agent_vars_dataframe()
-    agent_ids = list(agent_data.index.get_level_values('AgentID').unique())
-    mask = agent_data.index.get_level_values('AgentID').isin(agent_ids)
-    agents_data = agent_data[mask]
-    # Plot the battery_level column for each selected agent on the same plot
-    agents_data['recharge_value'].unstack().plot(linewidth=0.5, legend=None)
-    plt.show()
-
-    stop = timeit.default_timer()
-    print('Total Runtime: ', stop - start)
+    #
+    # agent_data = model.datacollector.get_agent_vars_dataframe()
+    # agent_ids = list(agent_data.index.get_level_values('AgentID').unique())
+    # mask = agent_data.index.get_level_values('AgentID').isin(agent_ids)
+    # agents_data = agent_data[mask]
+    # # Plot the battery_level column for each selected agent on the same plot
+    # agents_data['recharge_value'].unstack().plot(linewidth=0.5, legend=None)
+    # plt.show()
+    #
+    # stop = timeit.default_timer()
+    # print('Total Runtime: ', stop - start)
