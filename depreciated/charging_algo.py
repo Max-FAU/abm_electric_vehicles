b897274aef894e282929d20b3c17daf503b9144e
Max-FAU <maximilian_brueggemann@web.de>
2023-06-14 23:56:41 +0200

    charging algo in dump

--- depreciated/charging_algo.py
+++ depreciated/charging_algo.py
@@ -2,48 +2,53 @@ import pandas as pd
 import random
 
 
-capacity = 5000
-n = 1000
 
-df_1 = pd.DataFrame({"values": [random.randint(0, 7) for _ in range(n)],
-                     "final": [False] * n
-                     })
-distributed = 0
-
-while True:
-    completed_agents = len(df_1[df_1['final'] == True])
-    available_capacity = capacity - distributed
-
-    remaining_agents = len(df_1) - completed_agents
-
-    if remaining_agents > 0:
-        charging_value_per_agent = available_capacity / remaining_agents
-    else:
-        charging_value_per_agent = 0
-
-    for index, row in df_1.iterrows():
-        if not row['final']:
-            value = min(charging_value_per_agent, row['values'])
-            if value >= row['values']:
-                df_1.loc[index, 'values'] = value
-                df_1.loc[index, 'final'] = True
-                distributed += value
-
-    completed_agents_after = len(df_1[df_1['final'] == True])
-
-    if completed_agents == completed_agents_after:
+def charging_algo(capacity, df):
+    distributed = 0
+    while True:
+        completed_agents = len(df[df['final'] == True])
         available_capacity = capacity - distributed
-        # if all agents are below in the first step it will be 4 - 4 = 0
-        remaining_agents = len(df_1) - completed_agents_after
+
+        remaining_agents = len(df) - completed_agents
+
         if remaining_agents > 0:
             charging_value_per_agent = available_capacity / remaining_agents
-            # reduce all remaining agents
-            for index, row in df_1[df_1['final'] == False].iterrows():
-                df_1.loc[index, 'values'] = charging_value_per_agent
-                df_1.loc[index, 'final'] = True
-                distributed += charging_value_per_agent
-        break
+        else:
+            charging_value_per_agent = 0
 
-print(df_1)
-print(capacity)
-print(distributed)
+        for index, row in df.iterrows():
+            if not row['final']:
+                value = min(charging_value_per_agent, row['values'])
+                if value >= row['values']:
+                    df.loc[index, 'values'] = value
+                    df.loc[index, 'final'] = True
+                    distributed += value
+
+        completed_agents_after = len(df[df['final'] == True])
+
+        if completed_agents == completed_agents_after:
+            available_capacity = capacity - distributed
+            # if all agents are below in the first step it will be 4 - 4 = 0
+            remaining_agents = len(df) - completed_agents_after
+            if remaining_agents > 0:
+                charging_value_per_agent = available_capacity / remaining_agents
+                # reduce all remaining agents
+                for index, row in df[df['final'] == False].iterrows():
+                    df.loc[index, 'values'] = charging_value_per_agent
+                    df.loc[index, 'final'] = True
+                    distributed += charging_value_per_agent
+            break
+
+    return df
+
+if __name__ == '__main__':
+    capacity = 7
+    n = 5
+
+    charging_values = pd.DataFrame({"values": [random.randint(0, 10) for _ in range(n)],
+                                   "final": [False] * n
+                                   })
+    print(charging_values)
+    charging_values_new = charging_algo(capacity=capacity, df=charging_values)
+
+    print(charging_values_new)
